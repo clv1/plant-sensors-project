80b14c2abfcc7fc2f2d480bf6ad9bef3f2176276
Kevin <89152728+clv1@users.noreply.github.com>
2023-12-19 17:10:30 +0000

    Reordering table creation, correct foreign keys.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-USE plants; 
+USE plants;
 GO
 
 DROP TABLE s_alpha.plant;
@@ -7,44 +7,9 @@ DROP TABLE s_alpha.botanist;
 DROP TABLE s_alpha.origin_location;
 GO
 
-CREATE TABLE s_alpha.plant (
-    plant_id INT IDENTITY(1,1) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    scientific_name VARCHAR(100) NOT NULL,
-    origin_location_id INT NOT NULL,
-    image_url VARCHAR(300) NOT NULL,
-    PRIMARY KEY (plant_id)
-);
-GO
-
-CREATE TABLE s_alpha.recording_event (
-    recording_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    plant_id INT NOT NULL,
-    botanist_id INT NOT NULL,
-    soil_moisture FLOAT NOT NULL,
-    temperature FLOAT NOT NULL,
-    recording_taken DATETIME NOT NULL,
-    last_watered DATETIME NOT NULL
-    PRIMARY KEY (recording_id),
-    FOREIGN KEY (plant_id) REFERENCES plant(plant_id),
-    FOREIGN KEY (botanist_id) REFERENCES botanist(botanist_id)
-    );
-GO
-
-
-CREATE TABLE s_alpha.botanist (
-    botanist_id BIGINT IDENTITY(1,1) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(30) NOT NULL,
-    PRIMARY KEY (botanist_id)
-    );
-GO
-
 
 CREATE TABLE s_alpha.origin_location (
-    origin_location_id BIGINT IDENTITY(1,1) NOT NULL,
+    origin_location_id INT IDENTITY(1,1) NOT NULL,
     longitude FLOAT NOT NULL,
     latitude FLOAT NOT NULL,
     town VARCHAR(50) NOT NULL,
@@ -53,4 +18,40 @@ CREATE TABLE s_alpha.origin_location (
     continent VARCHAR(50) NOT NULL,
     PRIMARY KEY (origin_location_id)
 );
+GO
+
+
+CREATE TABLE s_alpha.plant (
+    plant_id INT IDENTITY(1,1) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    origin_location_id INT NOT NULL,
+    image_url VARCHAR(300) NULL,
+    PRIMARY KEY (plant_id),
+    FOREIGN KEY (origin_location_id) REFERENCES s_alpha.origin_location(origin_location_id)
+);
+GO
+
+CREATE TABLE s_alpha.botanist (
+    botanist_id INT IDENTITY(1,1) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(30) NOT NULL,
+    PRIMARY KEY (botanist_id)
+    );
+GO
+
+CREATE TABLE s_alpha.recording_event (
+    recording_id INT NOT NULL IDENTITY(1, 1),
+    plant_id INT NOT NULL,
+    botanist_id INT NOT NULL,
+    soil_moisture FLOAT NOT NULL,
+    temperature FLOAT NOT NULL,
+    recording_taken DATETIME NOT NULL,
+    last_watered DATETIME NOT NULL,
+    PRIMARY KEY (recording_id),
+    FOREIGN KEY (plant_id) REFERENCES s_alpha.plant(plant_id),
+    FOREIGN KEY (botanist_id) REFERENCES s_alpha.botanist(botanist_id)
+    );
 GO
