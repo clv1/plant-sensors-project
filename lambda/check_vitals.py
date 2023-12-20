c91dfe5cac323db6ced25f1ffd0c66b6b735dd32
anukrr <argkrr@gmail.com>
2023-12-20 09:28:42 +0000

    updated functionality for lambda function.

--- lambda/check_vitals.py
+++ lambda/check_vitals.py
@@ -1,3 +1,4 @@
+'''Prepares string for use in handler function.'''
 from os import environ
 from dotenv import load_dotenv
 import pandas as pd
@@ -9,26 +10,27 @@ temp_range_continent = {'America': [20, 35], 'Africa': [
 moisture_limit = [20, 60]
 
 
-def check_plant_vitals(plant: dict):
-    continent = plant.get('continent')
-    temp = plant.get('temperature')
-    moisture = plant['soil_moisture']
+def check_plant_vitals(df: pd.core.frame.DataFrame) -> list[dict]:
+    '''Checks if a plants vitals are healthy, if not unhealthy plants are returned as a list.'''
+    unhealthy_plants = []
+    for index, plant in df.iterrows():
+        continent = plant.get('continent')
+        temp = plant.get('temperature')
+        moisture = plant['soil_moisture']
+        if continent in temp_range_continent:
+            temp_range = temp_range_continent.get(f'{continent}')
+            if not temp_range[0] <= temp or temp <= temp_range[1] and moisture_limit[0] <= moisture <= moisture_limit[1]:
+                unhealthy_plants.append({
+                    'plant_id': plant['plant_id'],
+                    'temperature': temp,
+                    'soil_moisture': moisture,
+                    'botanist_email': plant['email'],
+                    'optimum_temp': temp_range,
+                    'continent': continent
+                })
+    return unhealthy_plants
 
-    if continent in temp_range_continent:
-        temp_range = temp_range_continent.get(f'{continent}')
-        if temp_range[0] >= temp or temp >= temp_range[1] or moisture_limit[0] >= moisture >= moisture_limit[1]:
-            print("plant is unhealthy")
-            return {
-                'plant_id': plant['plant_id'],
-                'temperature': temp,
-                'soil_moisture': moisture,
-                'botanist_email': plant['email']
-            }
-    return 'plant healthy'
+alerting_plants = check_plant_vitals(df)
 
-
-for _, plant in df:
-    fault_string = check_plant_vitals(plant)
-
-    def handler(event=None, context=None):
-        return {'message': fault_string}
+def handler(event=None, context=None):
+    return {'message': alerting_plants}
