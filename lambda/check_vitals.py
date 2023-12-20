fc03f927aeb9c28cd5802472ab0a19974e625270
anukrr <argkrr@gmail.com>
2023-12-20 16:11:33 +0000

    lambda function enhanced: email functionality and checking works now

--- lambda/check_vitals.py
+++ lambda/check_vitals.py
@@ -5,6 +5,29 @@ import pandas as pd
 import pprint
 import json
 import boto3
+import pyodbc
+
+
+def get_db_connection():
+    """Connects to the database"""
+    load_dotenv()
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={environ["DB_HOST"]};DATABASE={environ["DB_NAME"]};UID={environ["DB_USER"]};PWD={environ["DB_PASSWORD"]}'
+    # Establish a connection to the database
+    try:
+        connection = pyodbc.connect(conn_str)
+        print("Connected to the database successfully!")
+        return connection
+    except pyodbc.Error as e:
+        print("Error in connection: ", e)
+
+
+connection = get_db_connection()
+
+with connection.cursor() as curr:
+    curr.execute("""USE plants;""")
+    curr.execute("""SELECT * FROM s_alpha.botanist;""")
+    data = curr.fetchall()
+
 
 # Temporary...will use a database connection :)
 df = pd.read_csv('example_dataset.csv', index_col=0)
@@ -26,9 +49,7 @@ MOISTURE_LIMIT = [5, 60]
 # if temp is += 3˚C
 # moisture should not be below -> 0%-5% or >60% (temporary parameters that can be easily adjusted after seeing more data)
 
-
 client = boto3.client('ses', region_name='eu-west-2')
-
 
 def check_plant_vitals(df: pd.DataFrame) -> list[dict]:
     '''Checks if a plants vitals are healthy, if not unhealthy plants are returned as a list.'''
@@ -52,39 +73,61 @@ def check_plant_vitals(df: pd.DataFrame) -> list[dict]:
     return unhealthy_plants
 
 
-def send_email():
+
+def send_email(unhealthy_plants:list[dict]):
+    plant_warning  = generate_html_string(unhealthy_plants)
+   
     response = client.send_email(
         Destination={
-            'ToAddresses': ['trainee.anurag.kaur@sigmalabs.co.uk']
+            'ToAddresses': ['trainee.anurag.kaur@sigmalabs.co.uk', 'trainee.ishika.madhav@sigmalabs.co.uk']
         },
         Message={
             'Body': {
-                'Text': {
+                'Html': {
                     'Charset': 'UTF-8',
-                    'Data': 'This is the message body in text format.',
+                    'Data': plant_warning
                 }
             },
             'Subject': {
                 'Charset': 'UTF-8',
-                'Data': 'Unhealthy Plant',
+                'Data': 'Unhealthy Plant Detected by Sensor.',
             },
         },
-        Source='SourceEmailAddress'
+        Source='trainee.anurag.kaur@sigmalabs.co.uk'
     )
 
-    print(response)
+    return response
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps("Email Sent Successfully. MessageId is: " + response['MessageId'])
-    }
+def generate_html_string(plants:list[dict]) -> str:
+    """Generates HTML string for the email"""
+
+    warning_string = '<body>'
+
+    for plant in plants:
+        
+        plant_id = plant.get('plant_id')
+        temperature = plant.get('temperature')
+        optimum_temp = plant.get('optimum_temp')
+
+        if temperature > optimum_temp[1]:
+            difference = temperature - optimum_temp[1]
+            warning_string += f""" <li> Plant {plant_id} is above optimum temperature by {difference}. The optimum temperature range is {optimum_temp} but the temperature is {temperature} </li>"""
+        
+        if temperature < optimum_temp[0]:
+            difference = optimum_temp[0] - temperature
+            warning_string += f""" <li> Plant {plant_id} is below optimum temperature by {difference}. The optimum temperature range is {optimum_temp} but the temperature is {temperature} </li>"""
+    
+    warning_string += '</body>'
+    return warning_string 
 
 
-# we want to send an email only IF there are unhealthy plants (i.e. unhealthy_plants list is empty)
-# Q: what does the message in the handler go to?
-def handler(event=None, context=None):
-    return {'message': check_plant_vitals(df)}
+# def handler(event=check_plant_vitals(df), context=None): 
+#     if event:
+#         return send_email(event)
+#     return None
 
 
 if __name__ == "__main__":
-    pprint.pprint(check_plant_vitals(df))
+
+    json = check_plant_vitals(df)
+    send_email(json)
