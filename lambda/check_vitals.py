e77281ec5425d76da8e13202397fd9577abce7ee
anukrr <argkrr@gmail.com>
2023-12-21 16:57:28 +0000

    updated lambda + terraform for use with psycobd

--- lambda/check_vitals.py
+++ lambda/check_vitals.py
@@ -2,10 +2,11 @@
 from os import environ
 from dotenv import load_dotenv
 import pandas as pd
-import pprint
-import json
 import boto3
 import pyodbc
+from datetime import datetime, timedelta
+
+
 
 
 def get_db_connection():
@@ -23,25 +24,34 @@ def get_db_connection():
 
 connection = get_db_connection()
 
-with connection.cursor() as curr:
-    curr.execute("""USE plants;""")
-    curr.execute("""SELECT * FROM s_alpha.botanist;""")
-    data = curr.fetchall()
+def load_all_data(conn: pyodbc.Connection) -> pd.DataFrame:
+    """Loads all recording event data as a dataframe."""
+    query = """SELECT s_alpha.recording_event.*, s_alpha.plant.name, s_alpha.plant.image_url,
+                s_alpha.botanist.first_name, s_alpha.botanist.last_name,
+                s_alpha.origin_location.country
+                FROM s_alpha.recording_event
+                JOIN s_alpha.plant
+                ON s_alpha.plant.plant_id = s_alpha.recording_event.plant_id
+                JOIN s_alpha.botanist
+                ON s_alpha.botanist.botanist_id = s_alpha.recording_event.botanist_id
+                JOIN s_alpha.origin_location
+                ON s_alpha.origin_location.origin_location_id = s_alpha.plant.origin_location_id;"""
+    data = pd.read_sql(query, conn)
+    return data
 
+def load_current_data(connection):
+    df = load_all_data(connection)
+    current_time = datetime.now().minute
+    df = df[df['recording_taken'].dt.minute == current_time]
+    return df
 
-# Temporary...will use a database connection :)
-df = pd.read_csv('example_dataset.csv', index_col=0)
+def generate_avg_temp(df, plant:dict):
+    name = plant.get('name')
+    plant_df = df[df['name']==name]
+    sort_by_time = plant_df.sort_values(by='recording_taken', ascending=False)
+    last_recorded_values = sort_by_time.head(3)
+    return last_recorded_values['temperature'].mean()
 
-TEMP_RANGE_CONTINENT = {'America': [20, 35], 'Africa': [
-    25, 35], 'Asia': [25, 35], 'Europe': [9, 25], 'Pacific': [25, 35]}
-MOISTURE_LIMIT = [5, 60]
-
-# --- PREVIOUS IDEA
-# generate a baseline average temp and moisture data from
-# (previously extracted from a bunch of database readings)
-# set a range of +-10 (%)
-# what happens to moisture when plant is watered - does it shoot up
-# do some background research/visualisation to see existing patterns?
 
 # --- CURRENT PLAN FOR HEALTH CHECK
 # extract past 2-3 readings -> get average
@@ -51,27 +61,22 @@ MOISTURE_LIMIT = [5, 60]
 
 client = boto3.client('ses', region_name='eu-west-2')
 
+
 def check_plant_vitals(df: pd.DataFrame) -> list[dict]:
     '''Checks if a plants vitals are healthy, if not unhealthy plants are returned as a list.'''
     unhealthy_plants = []
     for index, plant in df.iterrows():
-        continent = plant.get('continent')
         temp = plant.get('temperature')
         moisture = plant.get('soil_moisture')
-
-        if continent in TEMP_RANGE_CONTINENT:
-            temp_range = TEMP_RANGE_CONTINENT.get(f'{continent}')
-            if not temp_range[0] <= temp or temp <= temp_range[1] and MOISTURE_LIMIT[0] <= moisture <= MOISTURE_LIMIT[1]:
-                unhealthy_plants.append({
-                    'plant_id': plant['plant_id'],
-                    'temperature': temp,
-                    'soil_moisture': moisture,
-                    'botanist_email': plant['email'],
-                    'optimum_temp': temp_range,
-                    'continent': continent
-                })
+        avg_temperature = int(generate_avg_temp(df, plant))
+        if (avg_temperature-3) >= temp or temp >= (avg_temperature+3):
+            unhealthy_plants.append({
+                'plant_id': plant['plant_id'],
+                'temperature': temp,
+                'soil_moisture': moisture,
+                'avg_temp': avg_temperature
+            })
     return unhealthy_plants
-
 
 
 def send_email(unhealthy_plants:list[dict]):
@@ -107,27 +112,23 @@ def generate_html_string(plants:list[dict]) -> str:
         
         plant_id = plant.get('plant_id')
         temperature = plant.get('temperature')
-        optimum_temp = plant.get('optimum_temp')
-
-        if temperature > optimum_temp[1]:
-            difference = temperature - optimum_temp[1]
-            warning_string += f""" <li> Plant {plant_id} is above optimum temperature by {difference}. The optimum temperature range is {optimum_temp} but the temperature is {temperature} </li>"""
+        avg = plant.get('avg_temp')
+        if temperature > avg:
+            difference = temperature - avg
+            warning_string += f""" <li> Plant {plant_id} is above optimum temperature by {difference}˚C. The average temperature is {avg}˚C but the temperature is {temperature}˚C </li>"""
         
-        if temperature < optimum_temp[0]:
-            difference = optimum_temp[0] - temperature
-            warning_string += f""" <li> Plant {plant_id} is below optimum temperature by {difference}. The optimum temperature range is {optimum_temp} but the temperature is {temperature} </li>"""
+        if temperature < avg:
+            difference = avg - temperature
+            warning_string += f""" <li> Plant {plant_id} is below optimum temperature by {difference}˚C. The average temperature is {avg}˚C but the temperature is {temperature}˚C</li>"""
     
     warning_string += '</body>'
     return warning_string 
 
+def handler(event, context=None):
+    connection = get_db_connection()
+    df = load_all_data(connection)
+    unhealthy_plants = check_plant_vitals(df)
+    if unhealthy_plants != []:
+        send_email(unhealthy_plants)
 
-def handler(event=check_plant_vitals(df), context=None): 
-    if event:
-        return send_email(event)
-    return None
 
-
-if __name__ == "__main__":
-
-    json = check_plant_vitals(df)
-    send_email(json)
